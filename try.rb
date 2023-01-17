f4e0c7a63cd8916e8582975008736d77b5edb213
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-01-17 10:50:53 -0600

    pine.fm thru GETS

---
+++ try.rb
@@ -0,0 +1,47 @@
+# puts "hello world"
+
+# # division returns the real math answer but ROUNDED DOWN
+# puts 100/60
+
+# # hours in a year
+# puts 24*365
+
+# #age of the person who wrote pine.fm starting in seconds
+# puts ((1406000000.00/60.00)/(60))/8760
+
+# puts 'go ' * 12
+
+# myString = 'this is great'
+# puts myString
+
+# name = "patricia Roasanna Jessia Mildred Oppenheimer"
+
+# puts 'My name is ' + name + '.'
+
+# var1=8
+# var2 =var1
+# puts var1
+# puts var2
+# puts 'BREAK'
+# var1='eight'
+# # var2=var1
+# puts var1
+# puts var2
+# puts 'END'
+
+# num1 = 2
+# num2 = '5'
+
+# puts num1.to_s + num2
+# name=gets.chomp
+
+# puts "hello " + name + ", you're doing great!"
+
+puts 'Hello!  What is your first name?'
+name = gets.chomp
+puts 'Oh! ' + name + ' is a fantastic name. but give me your full name!'
+name = gets.chomp 
+puts 'Got it.  Thanks ' + name + '. Now... what is your favorite number?'
+num = gets.chomp
+num = num.to_i+21
+puts 'Lame.  because ' + num.to_s + ' is actually a much better number.'
